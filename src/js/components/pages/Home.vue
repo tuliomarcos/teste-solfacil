62c4aafb293bedaf9acfbecacc1b0b5ecd1a81f9
@tuliosilva.iblue <tuliosilva.iblue@ciclic.com.br>
2021-11-26 19:29:58 -0300

    feature: finesh styles formalization session

--- src/js/components/pages/Home.vue
+++ src/js/components/pages/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="home-page" class="lg:pt-20 lg:pb-20 xl:pr-28 xl:pl-28 sm:pt-10 sm:pb-10 sm:pr-8 sm:pl-8 xs:pt-4 xs:pb-4 xs:pr-4 xs:pl-4">
-    <div id="formalization" class="flex justify-between">
+    <div id="formalization" class="flex sm:justify-between sm:items-center sm:flex-row xs:flex-col lg:pb-20 sm:pb-10 xs:pb-6">
       <div class="flex">
-        <svg class="w-16" xmlns="http://www.w3.org/2000/svg" width="64" height="65" viewBox="0 0 64 65" fill="none">
+        <svg class="md:w-16 xs:w-12" xmlns="http://www.w3.org/2000/svg" width="64" height="65" viewBox="0 0 64 65" fill="none">
           <circle cx="32" cy="32.5" r="31" stroke="#666666" stroke-width="2"/>
           <path d="M48 33.5C48.5523 33.5 49 33.0523 49 32.5C49 31.9477 48.5523 31.5 48 31.5L48 33.5ZM15.2929 31.7929C14.9024 32.1834 14.9024 32.8166 15.2929 33.2071L21.6569 39.5711C22.0474 39.9616 22.6805 39.9616 23.0711 39.5711C23.4616 39.1805 23.4616 38.5474 23.0711 38.1569L17.4142 32.5L23.0711 26.8431C23.4616 26.4526 23.4616 25.8195 23.0711 25.4289C22.6805 25.0384 22.0474 25.0384 21.6569 25.4289L15.2929 31.7929ZM48 31.5L16 31.5L16 33.5L48 33.5L48 31.5Z" fill="#666666"/>
         </svg>
         <div class="flex flex-col ml-4">
-          <h1 class="xl:text-4xl md:text-3xl">Formalizações</h1>
-          <h4>APROVADAS - {{ getCurrentDay() }} - {{ getCurrentHours() }}</h4>
+          <h1 class="font-normal xl:text-4xl xs:text-3xl">Formalizações</h1>
+          <h4 class="font-medium xs:text-base">APROVADAS - {{ getCurrentDay() }} - {{ getCurrentHours() }}</h4>
         </div>
       </div>
-      <button>GERAR CNAB</button>
+      <button class="font-medium bg-orange-primary text-gray-900 xs:py-3 xs:px-6 rounded text-sm">GERAR CNAB</button>
     </div>
     <div id="total-formalizations">
       <h1>Total formalizações</h1>
